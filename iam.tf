d46cd2e932d4c3a8dad473de132ecdb2d207a56a
Cloudfleet Technology Team <187966520+cloudfleet-tech@users.noreply.github.com>
2025-07-29 15:49:02 +0200

    Add europe-central-1a region

--- iam.tf
+++ iam.tf
@@ -1,7 +1,8 @@
 locals {
   cloudfleet_controller_aws_accounts = {
     "staging" : "891376988818"
-    "northamerica-central-1" : "891376988818"
+    "northamerica-central-1" : "891376988818",
+    "europe-central-1a": "891376988818"
   }
 }
 
